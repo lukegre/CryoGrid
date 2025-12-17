0500b8315bd6647aed728333964e929dcabd766f
lukegre <lukegre@gmail.com>
2025-12-17 16:59:07 +0200

    Add OUT_FDD_TDD2 and OUT_regridded2 classes where output display timestep can be controlled Refactored code

--- source/IO/RUN_INFO/RUN_SPATIAL_SPINUP_stagger.m
+++ source/IO/RUN_INFO/RUN_SPATIAL_SPINUP_stagger.m
@@ -5,7 +5,7 @@
 % which can run several TILE classes per point sequentially for model spin-up 
 %
 % S. westermann, Dec 2022
-% L. Gregor, Dec 2025 - Removed MULTITILE functionality, switched to parfor, added worker stagger start
+% L. Gregor, Dec 2025 - Removed MULTITILE functionality, refactored, added worker stagger start
 %========================================================================
 
 classdef RUN_SPATIAL_SPINUP_stagger < matlab.mixin.Copyable
@@ -18,10 +18,6 @@ classdef RUN_SPATIAL_SPINUP_stagger < matlab.mixin.Copyable
         SPATIAL
         TILE
         CLUSTER
-    end
-
-    properties (Constant)
-        stagger_interval = 10; %seconds to stagger worker start times
     end
 
     methods
@@ -38,6 +34,7 @@ classdef RUN_SPATIAL_SPINUP_stagger < matlab.mixin.Copyable
             
             run_info.PARA.projection_class = [];
             run_info.PARA.projection_class_index = [];
+            run_info.PARA.stagger_interval = 0; %seconds to stagger worker start times
                         
         end
         
@@ -62,44 +59,29 @@ classdef RUN_SPATIAL_SPINUP_stagger < matlab.mixin.Copyable
         end
 
         function [run_info, tile] = run_model(run_info)
-            tile = 0;
             if run_info.PARA.number_of_cores > 1
-                parpool(run_info.PARA.number_of_cores)
+                poolobj = gcp('nocreate'); % If no pool, do not create new one.
+                if isempty(poolobj)
+                    parpool(run_info.PARA.number_of_cores)
+                end
+
                 spmd
-                    [run_info, tile] = run_model_TILE_parallel_parfor(run_info);
+                    [run_info, tile] = run_model_parallel(run_info);
                 end
+                
+                delete(poolobj);
             else
-                [run_info, tile] = run_model_TILE_sequential(run_info);
+                [run_info, tile] = run_model_sequential(run_info);
             end
         end
 
-        function [run_info, tile] = run_model_TILE_parallel_parfor(run_info)
-            tile = 0;
-            number_spatial_points = size(run_info.SPATIAL.STATVAR.key, 1);
-            stagger = run_info.stagger_interval;
-
-            parfor run_number = 1:number_spatial_points
-                % Get the unique ID of the worker currently running this iteration
-                t = getCurrentTask(); 
-                worker_id = t.ID;
-
-                % Use persistent var that lives on the worker's process
-                persistent worker_initialized;  
-                if isempty(worker_initialized)  % block ONLY runs 1st time a worker executes code 
-                    pause_time = (worker_id - 1) * stagger;
-                    
-                    fprintf('Worker %d: First time initialization. Staggering %d sec.\n', worker_id, pause_time);
-                    pause(pause_time);
-                    
-                    worker_initialized = true;
-                end
-                run_spinup_for_range(run_info, worker_id, run_number, false);
-            end
-        end
-
-        function [run_info, tile] = run_model_TILE_parallel_spmd(run_info)
+        function [run_info, tile] = run_model_parallel(run_info)
             tile = 0;
             worker_number = spmdIndex();
+
+            pause_duration = (worker_number-1) * run_info.PARA.stagger_interval;
+            fprintf('Worker %d pausing for %d seconds to stagger start times...\n', worker_number, pause_duration);
+            pause(pause_duration); %stagger worker start times to reduce file access conflicts
 
             number_spatial_points = size(run_info.SPATIAL.STATVAR.key,1);
             max_number_of_gridcells = min(run_info.PARA.number_of_cores, size(run_info.SPATIAL.STATVAR.key,1));
@@ -110,21 +92,20 @@ classdef RUN_SPATIAL_SPINUP_stagger < matlab.mixin.Copyable
             if worker_number <= size(run_raster,1)
                 for run_number = run_raster(worker_number,1):run_raster(worker_number,2)
                     disp(['running grid cell ' num2str(run_number)])
-                    [run_info, tile] = run_spinup_for_range(run_info, worker_number, run_number, false);
+                    [run_info, tile] = run_TILE(run_info, worker_number, run_number);
                 end
             end
         end
 
-        function [run_info, tile] = run_model_TILE_sequential(run_info)
+        function [run_info, tile] = run_model_sequential(run_info)
             tile = 0;
             for run_number = 1:size(run_info.SPATIAL.STATVAR.key,1)
 
                 disp(['running grid cell ' num2str(run_number)])
-                [run_info, tile] = run_spinup_for_range(run_info, 1, run_number, true);
+                [run_info, tile] = run_TILE(run_info, 1, run_number);
             end
         end
 
-        %-------------param file generation-----
         function run_info = param_file_info(run_info)
             run_info = provide_PARA(run_info);
 
@@ -151,17 +132,18 @@ classdef RUN_SPATIAL_SPINUP_stagger < matlab.mixin.Copyable
     end
 
     methods (Access = private)
-        
-        function [run_info, tile] = run_spinup_for_range(run_info, worker_number, range, display_latlon)
+        function [run_info, tile] = run_TILE(run_info, worker_number, run_number)
             % Shared spin-up sequence across TILE parallel/sequential.
             tile = 0;
             for i=1:size(run_info.PARA.tile_class,1)
                 disp(['running tile number ' num2str(i)])
                 for j=1:run_info.PARA.number_of_runs_per_tile(i,1)
-                    disp(['running round ' num2str(j)])
+                    if j > 1
+                        disp(['running round ' num2str(j)])
+                    end
 
                     for ai=1:size(run_info.SPATIAL.ACTION,1)
-                        run_info.SPATIAL.ACTION{ai,1} = assign_tile_properties(run_info.SPATIAL.ACTION{ai,1}, range); %writes the provider class
+                        run_info.SPATIAL.ACTION{ai,1} = assign_tile_properties(run_info.SPATIAL.ACTION{ai,1}, run_number); %writes the provider class
                     end
 
                     new_tile = copy(run_info.PPROVIDER.CLASSES.(run_info.PARA.tile_class{i,1}){run_info.PARA.tile_class_index(i,1),1});
@@ -171,17 +153,15 @@ classdef RUN_SPATIAL_SPINUP_stagger < matlab.mixin.Copyable
                     run_info.TILE = tile;
 
                     tile.PARA.worker_number = worker_number;
-                    tile.PARA.range = range;
-
-                    if display_latlon
-                        fprintf('LAT: %.3f, LON: %.3f\n', tile.PARA.latitude, tile.PARA.longitude);
-                    end
+                    tile.PARA.range = run_number;
 
                     tile = run_model(tile);  %time integration
+                
                 end
             end
         end  % end of function
     end
+
 end
 
 
